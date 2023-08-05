84de0459863d9d3b8438382fb254b7cdaf192f22
amesp73 <amitrpatel502@gmail.com>
2023-08-05 15:00:58 -0500

    completed banner.js and updated app.js uo until step 5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import Banner from "./components/Banner";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import { useState } from "react";
+import satData from "./components/satData.js";
 
 
 function App() {
+  // sat compares changes in state, setSat is function
+  // that updates state
+
+  // Set method prevents duplicates
+  // if we only use map we would have 10 buttons many being duplicates
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) =>
+data.orbitType))];
+  const filterByType = (currentType) => {
+    
+  }
   return (
     <div>
+      <Banner />
       <Buttons />
       <Table />
     </div>
