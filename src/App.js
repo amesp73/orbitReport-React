a93bd1333c52a07afaa33f0d341dd9b6d6cd47da
amesp73 <amitrpatel502@gmail.com>
2023-08-06 12:04:48 -0500

    completed part 3 "update app.js"

--- src/App.js
+++ src/App.js
@@ -15,13 +15,19 @@ function App() {
   const displaySats = [...new Set(satData.map((data) =>
 data.orbitType))];
   const filterByType = (currentType) => {
-    
+      const displaySats = satData.filter((newSatDisplay) => {
+        return newSatDisplay.orbitType === currentType;
+      });
+      setSat(displaySats);
   }
   return (
     <div>
       <Banner />
       <Buttons />
-      <Table />
+        filterByType = {filterByType}
+        setSat = {setSat}
+        displaySats = {displaySats}
+      <Table sat = {sat}/>
     </div>
   );
 }
